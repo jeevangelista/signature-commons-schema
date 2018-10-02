d59f81f778c500290f07c209e8e55199ac547258
Daniel J. B. Clarke <u8sand@gmail.com>
2018-10-02 16:11:52 -0400

    Updated example tests to validate nested @context schemas

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,14 +1,17 @@
 import os
 import json
+from objectpath import Tree
 from urllib import request
 from jsonschema import Draft4Validator
 from jsonschema.exceptions import ValidationError
 
+fetch_cache = {}
 def fetch(url):
-  return json.load(request.urlopen(url))
+  global fetch_cache
+  fetch_cache[url] = fetch_cache.get(url, json.load(request.urlopen(url)))
+  return fetch_cache[url]
 
-def check_example(valid, example):
-  schema = fetch(example['$context'])
+def check_example(valid, example, schema):
   try:
     Draft4Validator(schema).validate(example)
     if not valid:
@@ -21,5 +24,6 @@ def test_examples():
   for root, dirs, files in os.walk('examples'):
     for f in files:
       examples = json.load(open(os.path.join(root, f), 'r'))
-      for example in examples['tests']:
-        yield check_example, examples['valid'], example
+      for ctx in Tree(examples).execute('$..*[@.@context is not None]'):
+        schema = fetch(ctx['@context'])
+        yield check_example, examples['valid'], ctx, schema
