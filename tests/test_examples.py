c24715e31b7269e973f878030f9509bbc62cc767
Daniel J. B. Clarke <u8sand@gmail.com>
2018-10-02 11:43:10 -0400

    Extended tests to also check for catch invalid examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,18 +1,24 @@
 import os
 import json
 from urllib import request
-from jsonschema import validate
+from jsonschema import Draft4Validator
 
 def fetch(url):
   return json.load(request.urlopen(url))
 
-def check_example(example):
+def check_example(valid, example):
   schema = fetch(example['$context'])
-  validate(example, schema)
+  try:
+    Draft4Validator(schema).validate(example)
+    result = True
+  except:
+    result = False
+  assert result == valid
+
 
 def test_examples():
   for root, dirs, files in os.walk('examples'):
     for f in files:
       examples = json.load(open(os.path.join(root, f), 'r'))
-      for example in examples:
-        yield check_example, example
+      for example in examples['tests']:
+        yield check_example, examples['valid'], example
