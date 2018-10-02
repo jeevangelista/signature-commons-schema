c177376dd37bae99f04af5072290fe1fec10466c
Daniel J. B. Clarke <u8sand@gmail.com>
2018-10-02 16:57:24 -0400

    Validate the examples formatting themselves

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -11,10 +11,13 @@ def fetch(url):
   fetch_cache[url] = fetch_cache.get(url, json.load(request.urlopen(url)))
   return fetch_cache[url]
 
+def context_validation(ctx):
+  schema = fetch(ctx['@context'])
+  Draft4Validator(schema).validate(ctx)
+
 def deep_validation(data):
   for ctx in Tree(data).execute('$..*[@.@context is not None]'):
-    schema = fetch(ctx['@context'])
-    Draft4Validator(schema).validate(ctx)
+    context_validation(ctx)
 
 def check_example(valid, example):
   try:
@@ -27,5 +30,7 @@ def test_examples():
   for root, dirs, files in os.walk('examples'):
     for f in files:
       examples = json.load(open(os.path.join(root, f), 'r'))
+      assert examples.get('@context') == 'https://raw.githubusercontent.com/dcic/signature-commons-schema/master/core/examples.json'
+      context_validation(examples)
       for example in examples['tests']:
         yield check_example, examples['valid'], example
