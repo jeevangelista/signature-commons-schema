6fbe28d462caf35755c54aa9d1d95948414b8682
Daniel J. B. Clarke <u8sand@gmail.com>
2018-10-03 10:54:41 -0400

    Delegated validation functions to `validator.py`

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,23 +1,6 @@
 import os
 import json
-from objectpath import Tree
-from urllib import request
-from jsonschema import Draft4Validator
-from jsonschema.exceptions import ValidationError
-
-fetch_cache = {}
-def fetch(url):
-  global fetch_cache
-  fetch_cache[url] = fetch_cache.get(url, json.load(request.urlopen(url)))
-  return fetch_cache[url]
-
-def context_validation(ctx):
-  schema = fetch(ctx['@context'])
-  Draft4Validator(schema).validate(ctx)
-
-def deep_validation(data):
-  for ctx in Tree(data).execute('$..*[@.@context is not None]'):
-    context_validation(ctx)
+from validator import deep_validation, context_validation
 
 def check_example(valid, example):
   try:
--- tests/test_meta.py
+++ tests/test_meta.py
@@ -1,9 +1,9 @@
 import os
 import json
-from jsonschema import Draft4Validator
+from validator import schema_validation
 
 def test_meta_schemas():
   for root, dirs, files in os.walk('meta'):
     for f in files:
       schema = json.load(open(os.path.join(root, f), 'r'))
-      yield Draft4Validator.check_schema, schema
+      yield schema_validation, schema
