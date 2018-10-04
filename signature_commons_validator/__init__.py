b51a215a470df224e727592881efe3dad0a02f3b
Daniel J. B. Clarke <u8sand@gmail.com>
2018-10-04 12:33:32 -0400

    Updated validation to utilize @context & @type

--- signature_commons_validator/__init__.py
+++ signature_commons_validator/__init__.py
@@ -9,13 +9,15 @@ def fetch(url):
   fetch_cache[url] = fetch_cache.get(url, json.load(request.urlopen(url)))
   return fetch_cache[url]
 
-def context_validation(ctx):
-  schema = fetch(ctx['@context'])
-  Draft4Validator(schema).validate(ctx)
+def context_validation(ctx, typ, data):
+  schema = fetch(ctx + typ)
+  Draft4Validator(schema).validate(data)
 
 def deep_validation(data):
+  # NOTE: This misses @type's that don't have a parent @context
   for ctx in Tree(data).execute('$..*[@.@context is not None]'):
-    context_validation(ctx)
+    for data in Tree(ctx).execute('$..*[@.@type is not None]'):
+      deep_validation(ctx['@context'], data['@type'], data)
 
 def schema_validation(schema):
   Draft4Validator.check_schema(schema)
