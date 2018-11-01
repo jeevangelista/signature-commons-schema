3dc78c298cd7661a52e0f524eb390ed4418e91d2
Daniel J. B. Clarke <u8sand@gmail.com>
2018-11-01 10:01:34 -0400

    Added tests for an inline validator

---
+++ src/validate/validate.standalone-inline.test.ts
@@ -0,0 +1,56 @@
+import { strict as assert } from 'assert'
+import { validate } from '.'
+
+const testJSON = {
+  "tests": [
+    {
+      "name": "simple pass",
+      "valid": true,
+      "schema": "meta",
+      "data": {
+        "$validator": {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "number"
+            }
+          }
+        },
+        "id": 0
+      }
+    },
+    {
+      "name": "simple fail",
+      "valid": false,
+      "schema": "meta",
+      "data": {
+        "$validator": {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "number"
+            }
+          }
+        },
+        "id": "test"
+      }
+    }
+  ]
+}
+
+describe('util', () => {
+  describe('validate', () => {
+    describe('standalone inline validation', () => {
+      for(const test of testJSON.tests) {
+        it(test.name, async () => {
+          try {
+            const result = await validate(test.data)
+            assert.equal(test.valid, true, 'Validated successfully')
+          } catch(e) {
+            assert.equal(test.valid, false, JSON.stringify(e.errors))
+          }
+        })
+      }
+    })
+  })
+})
