524ced58fc81dd55cba909a294bfce700ff4170a
Daniel J. B. Clarke <u8sand@gmail.com>
2018-11-01 11:35:48 -0400

    Enable fetch_cached to obtain npm-defined locals given `/` prefix

--- src/util/fetch-cached.ts
+++ src/util/fetch-cached.ts
@@ -5,12 +5,27 @@ const cache: {
 } = {}
 
 /**
- * Fetch object from remote, caching any results
+ * Fetch object from remote, caching any results.
+ *  Treats `^/` as an `import`,
+ *  @dcic/signature-commons-schema pointing to the local tree
+ * 
  * @param url The url to fetch the object from
  */
 export async function fetch_cached<T extends {} = {}>(url: string): Promise<T> {
-  if(cache[url] === undefined)
+  if(url.indexOf('/') === 0) {
+    // Fetch url's that start with / as impots
+    cache[url] = (
+      await import(
+        url.replace(
+          /^\/@dcic\/signature-commons-schema/,
+          '/../../'
+        ).substr(1)
+      )
+    ) as T
+  } else if(cache[url] === undefined) {
+    // Fetch other urls as actual urls
     cache[url] = await (await fetch(url)).json()
+  }
 
   return cache[url] as T
 }
