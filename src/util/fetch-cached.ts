d25d3c98e27f31ade247aff7be668f7b457e7794
Daniel J. B. Clarke <u8sand@gmail.com>
2019-06-03 16:55:37 -0400

    Added missed v4's

--- src/util/fetch-cached.ts
+++ src/util/fetch-cached.ts
@@ -17,7 +17,7 @@ export async function fetch_cached<T extends {} = {}>(url: string): Promise<T> {
     // Fetch url's that start with / as impots
     const m = /^\/@dcic\/signature-commons-schema(\/v\d+)?(.*)$/.exec(url)
     if (m) {
-      if (m[1] === '/v3') {
+      if (m[1] === '/v4') {
         cache[url] = await import(`../..${m[2]}`)
       } else if (m[1] === undefined) {
         url = `https://raw.githubusercontent.com/dcic/signature-commons-schema/v1${m[2]}`
